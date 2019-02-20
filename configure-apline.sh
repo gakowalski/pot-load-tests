5e596f887dd70f5c7899f371f3b32c5fd8457e5d
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-02-20 17:39:21 +0100

    fix for running script, cleaner install

--- configure-apline.sh
+++ configure-apline.sh
@@ -3,14 +3,12 @@ apk add docker
 rc-update add docker boot
 service docker start
 apk add git
-cd /opt
-mkdir gatling
-cd gatling
-mkdir results
-mkdir user-files
-mkdir user-files/simulations
-mkdir conf
-cd user-files/simulations
+mkdir /opt/gatling
+mkdir /opt/gatling/results
+mkdir /opt/gatling/user-files
+mkdir /opt/gatling/user-files/simulations
+mkdir /opt/gatling/conf
+cd /opt/gatling/user-files/simulations
 git clone https://github.com/gakowalski/pot-load-tests
 cp /opt/gatling/user-files/simulations/pot-load-tests/run-gatling.sh /opt/gatling
 cd /opt/gatling
