47b2d07151a2ccca5f4e7a191d30bff0af978fda
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-02-20 18:17:50 +0100

    random switches

--- polskazobaczwiecej/PrzegladanieOfert.scala
+++ polskazobaczwiecej/PrzegladanieOfert.scala
@@ -3,7 +3,7 @@ package polskazobaczwiecej
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import scala.util.Random
+import java.util.concurrent.ThreadLocalRandom
 
 class PrzegladanieOfert extends Simulation {
 
@@ -12,21 +12,33 @@ class PrzegladanieOfert extends Simulation {
     .doNotTrackHeader("1")
   	.acceptEncodingHeader("gzip, deflate")
 
-  def randomRegion() = Random.nextInt(15) + 1
+  def randomRegion() = ThreadLocalRandom.current.nextInt(15) + 1
+
+  val czytajRegulamin = exec(http("Regulamin").get("/REGULAMIN-wiosna-2019.pdf").check(status.not(404))).pause(1, 20)
+  var czytajInformacje = exec(http("Informacje").get("/informacje").check(status.not(404))).pause(1, 10)
+
+  val uwaznyInternauta = randomSwitch(
+    	10d -> exec(czytajRegulamin).exec(czytajInformacje),
+    	90d -> exec(czytajInformacje)
+    )
+
+  val sprawdzOferty = exec(http("Sprawdź oferty").get("/lista-obiektow").check(status.not(404)))
+  	.pause(15, 30)
+  	.exec(http("Sprawdz oferty").get("/lista-obiektow/strona-2").check(status.not(404)))
+  	.pause(15, 30)
+  	.exec(http("Filtruj").get("/lista-obiektow?s=&r={randomRegion()}&m=").check(css("div.ob-list h3 > a", "href").saveAs("oferta")))
+  	.pause(3, 10)
+  	.exec(http("Szczegoly oferty").get("${oferta}"))
+  	.pause(15, 45)
 
   val scn = scenario("Sprawdz oferty")
   	.exec(http("Strona glowna").get("/").check(status.not(404)))
   	.pause(1, 10)
-  	.exec(http("Informacje").get("/informacje").check(status.not(404)))
-  	.pause(1, 10)
-  	.exec(http("Regulamin").get("/REGULAMIN-wiosna-2019.pdf").check(status.not(404)))
-  	.pause(1, 20)
-  	.exec(http("Sprawdz oferty").get("/lista-obiektow").check(status.not(404)))
-  	.pause(3, 15)
-  	.exec(http("Filtruj").get("/lista-obiektow?s=&r={randomRegion()}&m=").check(css("div.ob-list h3 > a", "href").saveAs("oferta")))
-  	.pause(3, 10)
-  	.exec(http("Szczegoly oferty").get("${oferta}"))
-  	.pause(3, 15)
+  	.randomSwitch(
+    	50d -> exec(uwaznyInternauta).exec(sprawdzOferty),
+  		50d -> exec(sprawdzOferty)
+    )
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+
+  setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
 }
