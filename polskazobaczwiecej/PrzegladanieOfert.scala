3da2daa49bcf739e39f63a471b5da0e8fa62cae6
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-02-21 16:22:58 +0100

    Fix for zero offers as result of search

--- polskazobaczwiecej/PrzegladanieOfert.scala
+++ polskazobaczwiecej/PrzegladanieOfert.scala
@@ -3,7 +3,8 @@ package polskazobaczwiecej
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import java.util.concurrent.ThreadLocalRandom
+import scala.util.Random
+import java.lang.System
 
 class PrzegladanieOfert extends Simulation {
 
@@ -12,9 +13,10 @@ class PrzegladanieOfert extends Simulation {
     .doNotTrackHeader("1")
   	.acceptEncodingHeader("gzip, deflate")
 
-  def randomRegion() = ThreadLocalRandom.current.nextInt(15) + 1
-  def randomService() = ThreadLocalRandom.current.nextInt(9) + 1
-  def randomOffer() = ThreadLocalRandom.current.nextInt(9)
+  val random = new Random(System.currentTimeMillis())
+
+  def randomService() = random.nextInt(9) + 1
+  def randomInt(min : Int, max : Int) = random.nextInt(max - min) + min
 
   val czytajRegulamin = exec(http("Regulamin").get("/REGULAMIN-wiosna-2019.pdf").check(status.not(404))).pause(1, 20)
   var czytajInformacje = exec(http("Informacje").get("/informacje").check(status.not(404))).pause(1, 10)
@@ -24,28 +26,38 @@ class PrzegladanieOfert extends Simulation {
     	90d -> exec(czytajInformacje)
     )
 
-  val sprawdzOferty = exec(http("Sprawdz oferty (str. 1)").get("/lista-obiektow").check(status.not(404)))
+  val sprawdzOferty = exec(
+      http("Sprawdz oferty (str. 1)").get("/lista-obiektow")
+      .check(
+        status.not(404),
+        css("option+option", "value").findAll.saveAs("wojewodztwa")
+      )
+    )
   	.pause(15, 30)
   	.exec(http("Sprawdz oferty (str. 2)").get("/lista-obiektow/strona-2").check(status.not(404)))
   	.pause(15, 30)
-  	.exec(http("Filtruj (woj.)").get("/lista-obiektow?s=&r={randomRegion()}&m=").check(css("div.ob-list h3 > a", "href").find(randomOffer()).saveAs("oferta_1")))
+  	.exec(http("Filtruj (woj.)").get("/lista-obiektow?s=&r=${wojewodztwa.random()}&m=").check(css("div.ob-list h3 > a", "href").find(randomOffer()).saveAs("oferta_1")))
   	.pause(5, 15)
   	.exec(http("Szczegoly oferty #1").get("${oferta_1}"))
   	.pause(15, 45)
     .exec(http("Filtruj (woj. + usluga)").get("/lista-obiektow")
       .queryParam("s", "")
-      .queryParam("r", randomRegion())
+      .queryParam("r", "${wojewodztwa.random()}")
       .queryParam("m", "")
       .queryParam("c[]", randomService())
       .check(css("div.ob-list h3 > a", "href")
-      .find(randomOffer())
-      .saveAs("oferta_2"))
+      .findAll
+      .optional
+      .saveAs("oferty"))
     )
   	.pause(5, 15)
-    .exec(http("Szczegoly oferty #2").get("${oferta_2}"))
-  	.pause(15, 45)
+    .doIf("${oferty.exists()}") {
+      exec(http("Szczegoly oferty #2").get("${oferty.random()}"))
+    	.pause(15, 45)
+    }
 
-  val scn = scenario("Przegladane Ofert")
+
+  val scn = scenario("Przegladanie Ofert")
   	.exec(http("Strona glowna").get("/").check(status.not(404)))
   	.pause(1, 10)
   	.randomSwitch(
@@ -53,6 +65,7 @@ class PrzegladanieOfert extends Simulation {
   		50d -> exec(sprawdzOferty)
     )
 
+  val nbUsers = Integer.getInteger("users", 10)
 
-  setUp(scn.inject(atOnceUsers(10)).protocols(httpConf))
+  setUp(scn.inject(atOnceUsers(nbUsers)).protocols(httpConf))
 }
